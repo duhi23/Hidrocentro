1fd23159ba2b65a5a717016d1e4572c9991883ab
Diego Huaraca <diego.21.phs@hotmail.com>
2015-06-21 22:26:30 -0500

    Execute

---
+++ Script.R
@@ -0,0 +1,6 @@
+#####################################
+### Script de Ejecucion - Reporte ###
+#####################################
+
+system("xelatex -interaction=batchmode reporte ")
+embedFonts("reporte.pdf")
